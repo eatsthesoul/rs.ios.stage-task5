7aecdddf749b01020790d5520af07964477724ba
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-06-13 14:20:43 +0300

    feat: add solution for exercise-1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,25 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        
+        var profit = 0
+        var pricesArray = prices
+        
+        func countProfitBeforeMax() {
+            
+            guard let maxValue = pricesArray.max() else { return }
+            
+            for price in pricesArray {
+                pricesArray.remove(at: 0)
+                if price == maxValue { return }
+                profit += maxValue - price
+            }
+        }
+        
+        while pricesArray.count != 0 {
+            countProfitBeforeMax()
+        }
+        
+        return profit
     }
 }
